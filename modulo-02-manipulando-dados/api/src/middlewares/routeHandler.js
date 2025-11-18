6f06543b5df231727a80019c3ece302fd6f46725
kayandenizo <kayan.denizo@gmail.com>
2025-11-18 15:51:40 -0300

    feat: adicionar modulo-02-manipulando-dados com novos exercícios e estrutura inicial

---
+++ modulo-02-manipulando-dados/api/src/middlewares/routeHandler.js
@@ -0,0 +1,21 @@
+import { routes } from "../routes.js";
+import { extractQueryParams } from "../utils/extract-query-params.js";
+
+export function routeHandler(request, response) {
+    const route = routes.find((route) => {
+        return route.method === request.method && route.path.test(request.url)
+    })
+
+    if (route) {
+        const routeParams = request.url.match(route.path)
+
+        const { query, ...params } = routeParams.groups
+
+        request.query = query ? extractQueryParams(query) : {}
+        request.params = params
+
+        return route.controller(request, response)
+    }
+
+    return response.writeHead(404).end('Rota não encontrada');
+}
