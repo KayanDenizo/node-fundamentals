84371147ebe72a445c003d5dbecbbeeca7c2c80e
kayandenizo <kayan.denizo@gmail.com>
2025-11-18 16:49:45 -0300

    feat: Implementar criação de arquivo json de forma dinâmica usando o node.

--- modulo-02-manipulando-dados/api/src/database.js
+++ modulo-02-manipulando-dados/api/src/database.js
@@ -1,6 +1,18 @@
+import fs from "node:fs/promises"
+const DATABASE_PATH = new URL("db.json", import.meta.url)
+
 // Banco de dados 
 export class DataBase {
     database = {}
+
+    constructor() { //Executado automaticamente quando a classe é iniciada
+        this.persist()
+    }
+
+    persist() {
+        fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+    }
+
     insert(table, data) {
         if (Array.isArray(this.database[table])) {
             this.database[table].push(data)
