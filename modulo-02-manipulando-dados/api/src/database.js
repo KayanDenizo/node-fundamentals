26789574e1c56ba259a1ac7b7c00ed58a8d3be69
kayandenizo <kayan.denizo@gmail.com>
2025-11-18 17:13:08 -0300

    fix: corrigindo métodos e classe para serem privados

--- modulo-02-manipulando-dados/api/src/database.js
+++ modulo-02-manipulando-dados/api/src/database.js
@@ -3,32 +3,32 @@ const DATABASE_PATH = new URL("db.json", import.meta.url)
 
 // Banco de dados 
 export class DataBase {
-    database = {}
+    #database = {} //# significa que é privado, so esta disponivel dentro da classe
 
 
     constructor() { //Executado automaticamente quando a classe é iniciada
         fs.readFile(DATABASE_PATH, "utf8")
             .then((data) => {
-                this.database = JSON.parse(data)
+                this.#database = JSON.parse(data)
             })
-            .catch(() => this.persist())
+            .catch(() => this.#persist())
 
     }
 
-    persist() {
-        fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+    #persist() {
+        fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
     }
 
     insert(table, data) {
-        if (Array.isArray(this.database[table])) {
-            this.database[table].push(data)
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data)
         } else {
-            this.database[table] = [data] //Se nao existe adiciona o nome da tabela o primeiro objeto, q inclusiva eu ja paço na requisição no insominia
+            this.#database[table] = [data] //Se nao existe adiciona o nome da tabela o primeiro objeto, q inclusiva eu ja paço na requisição no insominia
         }
-        this.persist() // Salva o conteudo no arquivo json
+        this.#persist() // Salva o conteudo no arquivo json
     }
 
     select(table) {
-        return this.database[table]
+        return this.#database[table] ?? [] // Se tiver conteudo retorna o conteudo, caso contrario (??) retorna uma lista (array) vazia
     }
 }
